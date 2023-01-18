22fb64002a4812d7f8bbbc2d4319e176b9cc6d2a
Denis <beznikind@gmail.com>
2023-01-18 23:03:57 +0300

    Задать номер четверти, показать диапазоны для возможных координат

--- 003/Program.cs
+++ 003/Program.cs
@@ -1,2 +1,25 @@
 ﻿// Задать номер четверти, показать диапазоны для возможных координат
-Console.WriteLine("Hello, World!");
+
+Console.Write("Введите  четверть координат - ");
+int number = Int32.Parse(Console.ReadLine());
+
+if (number == 1 ) 
+{
+Console.WriteLine(" X ( 1 ; infinity) ; Y( 1 ; infinity ) ");
+}
+if (number == 2 ) 
+{
+Console.WriteLine(" X ( -1 ; -infinity) ; Y( 1 ; infinity ) ");
+}
+if (number == 3 ) 
+{
+Console.WriteLine(" X ( -1 ; -infinity) ; Y( -1 ; -infinity ) ");
+}
+if (number == 4 ) 
+{
+Console.WriteLine(" X ( 1 ; infinity) ; Y( -1 ; -infinity ) ");
+}
+if (number > 4 ) 
+{
+Console.WriteLine("Цифра должна быть от 1 до 4 ");
+}
